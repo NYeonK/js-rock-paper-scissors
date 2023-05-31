f4619d473866cde2e695beb1067cb0561b601665
NYeonK <skdus728@gmail.com>
2023-06-01 00:51:03 +0900

    [ feat ] webpack asset Modules을 활용하여 image 가져오기

--- src/game-progress.js
+++ src/game-progress.js
@@ -1,3 +1,9 @@
 export default function gameProgress() {
+  const gameImage = document.querySelector(".gameImage");
+  for (let i = 1; i < 4; i++) {
+    const img = new Image();
+    img.src = `src/assets/img-${i}.png`;
+    gameImage.appendChild(img);
+  }
   return "hello, webpack";
 }
