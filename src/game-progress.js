fd7af1be48a801ef9f9ca6ba99f567798d2683ff
NYeonK <skdus728@gmail.com>
2023-06-06 19:58:16 +0900

    [ fix ] 'Uncaught SyntaxError: Unexpected token <' error 해결을 위한 test code 추가

--- src/game-progress.js
+++ src/game-progress.js
@@ -2,6 +2,8 @@ import ImgRock from "./assets/img-1.png";
 
 export default function gameProgress() {
   const gameImage = document.querySelector(".gameImage");
+  const test = document.querySelector("#test");
+  test.appendChild("안녕?");
   const img = new Image();
   img.src = ImgRock;
   gameImage.appendChild(img);
