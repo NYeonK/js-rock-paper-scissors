9f70d92343b8ba94037db56ecb078db69f007b25
NYeonK <skdus728@gmail.com>
2023-06-08 16:19:07 +0900

    [ etc ] test js file

--- src/game-progress.js
+++ src/game-progress.js
@@ -1,10 +1,12 @@
 //import ImgRock from "@/assets/img-1.png";
 
-export default function gameProgress() {
+export default function gameProgress(name) {
   //const gameImage = document.querySelector(".gameImage");
   const test = document.querySelector("#test");
   test.appendChild(document.createTextNode("안녕?"));
   //const img = new Image();
   //img.src = ImgRock;
   //gameImage.appendChild(img);
+
+  console.log("hello", name);
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@
 //import "./index.css";
 
 //gameProgress();
-console.log("안녕");
-const test = document.querySelector("#test");
-test.appendChild(document.createTextNode("안녕?"));
+// console.log("안녕");
+// const test = document.querySelector("#test");
+// test.appendChild(document.createTextNode("안녕?"));
+
+import { gameProgress } from "./game-progress";
+function myFunc() {
+  gameProgress("jack");
+  console.log("myFunc");
+}
+myFunc();
