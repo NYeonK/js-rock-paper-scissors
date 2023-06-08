7dc20cdca97dd19ce470f61066322a1df3f743d3
NYeonK <skdus728@gmail.com>
2023-06-08 16:01:09 +0900

    [ feat ] index.js에 test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import gameProgress from "./game-progress";
+//import gameProgress from "./game-progress";
 //import "./index.css";
 
-gameProgress();
+//gameProgress();
 console.log("안녕");
+const test = document.querySelector("#test");
+test.appendChild(document.createTextNode("안녕?"));
