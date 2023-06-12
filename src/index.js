092b1a8b44fdf9a2b5489428eea32141a537fddf
NYeonK <skdus728@gmail.com>
2023-06-13 02:30:32 +0900

    [ feat ] 가위바위보 게임 결과값 구현

--- src/index.js
+++ src/index.js
@@ -39,12 +39,33 @@ function changeGameImage() {
   }
 }
 
+const getGameResult = (computer, user) => {
+  const results = {
+    draw: "무승부",
+    win: "승리",
+    lose: "패배",
+  };
+
+  if (computer === user) {
+    return console.log(results.draw);
+  }
+
+  if (
+    (computer === 0 && user === 2) ||
+    (computer === 1 && user === 0) ||
+    (computer === 2 && user === 1)
+  ) {
+    return console.log(results.lose);
+  }
+
+  return console.log(results.win);
+};
+
 window.onload = function () {
   const interval = setInterval(changeGameImage, 300);
   userSelectedButton.forEach((button, index) => {
     button.addEventListener("click", () => {
-      console.log("컴퓨터 값: ", lastSelectImg);
-      console.log("사용자 값: ", index);
+      getGameResult(lastSelectImg, index);
       clearInterval(interval);
     });
   });
